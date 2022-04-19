4ae2d19efc3bf030d81253ad7a9ddf9a2be55d23
Elisee-des <esabidani@gmail.com>
2022-04-19 00:07:48 +0000

    contact ok.ecriture de la copie des addres

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,11 +1,51 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import emailjs, { init } from '@emailjs/browser';
+init(process.env.REACT_APP_ID)
 
 const ContactForm = () => {
+    const form = useRef();
+  
+    const sendEmail = (e) => {
+      e.preventDefault();
+  
+      const formMess = document.querySelector(".form-message")
+  
+      emailjs.sendForm("service_4bdqtiz", "template_hgxq536", form.current, "KH2PQNfUY8NjGIBJY")
+        .then((result) => {
+            console.log(result.text); 
+            form.current.reset()
+            formMess.innerHTML = "<p className='success'>Message Envoyé avec succes</p>";
+  
+            setTimeout(() => {
+                formMess.innerHTML = "";
+            }, 2500)
+        }, (error) => {
+            console.log(error.text);
+            formMess.innerHTML = "<p className='error'>Une erreur c'eest produit veuillez reessayé</p>";
+            
+            setTimeout(() => {
+                formMess.innerHTML = "";
+              }, 2500)
+        });
+    };
+  
     return (
-        <div>
-            <h1>Hello</h1>
+        <div className="form-container">
+            <h2>Contactez-nous</h2>
+          <form ref={form} onSubmit={sendEmail} className="form-content">
+              <label>Nom</label>
+              <input type="text" name="name" required id='name' />
+              <label>Email</label>
+              <input type="email" name="email" required id='email' />
+              <label>Message</label>
+              <textarea name="message"  required id='mess'/>
+              <input type="submit" value="Send" className='hover button'/>
+          </form>
+          <div className="form-message">
+  
+          </div>
         </div>
     );
-};
+  };
 
 export default ContactForm;
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,14 +2,35 @@ import React from 'react';
 import ContactForm from '../components/ContactForm';
 import Logo from '../components/Logo';
 import Navigation from '../components/Navigation';
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import Mouse from "../components/Mouse";
 
 const Projet1 = () => {
     return (
         <main>
+                <Mouse />
             <div className="contact"> 
                 <Navigation />
                 <Logo />
                 <ContactForm />
+                <div className="contact-infos">
+                    <div className="address">
+                        <div className="content">
+                            <h4>addresse</h4>
+                            <p>12 rue Laplace</p>
+                            <p>64500 Briazzte</p>
+                        </div>
+                    </div>
+                    <div className="phon">
+                        <div className="content">
+                            <h4>téléphone</h4>
+                            <CopyToClipboard text="22656885237" className='hover'>
+                                <p style={{ cursor: "pointer" }} className="clipboard"
+                                onClick={()=> alert("Telephone copié")} >56 88 52 37</p>
+                            </CopyToClipboard>
+                        </div>
+                    </div>
+                </div>
             </div>
         </main>
     );
